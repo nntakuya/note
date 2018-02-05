2e26493f7ccbe456d6af1c4e0fd9e73e0ad7699a
takuya <uv.nkmt.yii@gmail.com>
2018-02-05 11:03:16 +0800

    サイドバーのジェスチャー機能作成中

--- SlideMenuControllerSwift/BaseViewController.swift
+++ SlideMenuControllerSwift/BaseViewController.swift
@@ -93,3 +93,40 @@ class BaseViewController: UIViewController {
     */
 
 }
+
+
+
+extension BaseViewController : SlideMenuControllerDelegate {
+    
+    func leftWillOpen() {
+        print("SlideMenuControllerDelegate: leftWillOpen")
+    }
+    
+    func leftDidOpen() {
+        print("SlideMenuControllerDelegate: leftDidOpen")
+    }
+    
+    func leftWillClose() {
+        print("SlideMenuControllerDelegate: leftWillClose")
+    }
+    
+    func leftDidClose() {
+        print("SlideMenuControllerDelegate: leftDidClose")
+    }
+    
+    func rightWillOpen() {
+        print("SlideMenuControllerDelegate: rightWillOpen")
+    }
+    
+    func rightDidOpen() {
+        print("SlideMenuControllerDelegate: rightDidOpen")
+    }
+    
+    func rightWillClose() {
+        print("SlideMenuControllerDelegate: rightWillClose")
+    }
+    
+    func rightDidClose() {
+        print("SlideMenuControllerDelegate: rightDidClose")
+    }
+}
