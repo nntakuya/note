6904af98fc8cadc4acedffb6fa35cdf0017a37de
takuya <uv.nkmt.yii@gmail.com>
2018-02-05 10:25:07 +0800

    frist commit

---
+++ SlideMenuControllerSwift/BaseViewController.swift
@@ -0,0 +1,95 @@
+//
+//  BaseViewController.swift
+//  SlideMenuControllerSwift
+//
+//  Created by 仲松拓哉 on 04/02/2018.
+//  Copyright © 2018 Yuji Hato. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    
+    @IBOutlet weak var postView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //行間指定
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 2
+        let attributes = [NSAttributedStringKey.paragraphStyle : style]
+        postView.attributedText = NSAttributedString(string: postView.text,attributes: attributes)
+        
+        //表示テキストのフォントサイズを変更
+        postView.font = UIFont.systemFont(ofSize: 16)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setNavigationBarItem()
+    }
+    
+    
+    
+    @IBAction func keyClose(_ sender: UISwipeGestureRecognizer) {
+        postView.resignFirstResponder()
+    }
+    
+    //ToDo保留
+    func setInputAccessoryView() {
+        let kbToolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
+        kbToolBar.barStyle = UIBarStyle.default  // スタイルを設定
+        kbToolBar.sizeToFit()  // 画面幅に合わせてサイズを変更
+        // スペーサー
+        let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace,
+                                     target: self, action: nil)
+        
+        // 完了ボタン
+        let commitButton = UIBarButtonItem(
+            title: "Done",
+            style: .done,
+            target: self,
+            action: #selector(self.commitButtonTapped(sender:))
+        )
+        
+        kbToolBar.items = [spacer, commitButton]
+        
+        postView.inputAccessoryView = kbToolBar
+    }
+    
+    
+    //おそらく以下はボタンを押した後のアクション定義する場所
+    //@objcはググる
+    // #selectorをつけるから、
+    @objc func commitButtonTapped(sender: Any) {
+        self.resignFirstResponder()
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
